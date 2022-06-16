6efa09bda6262ef9e0660a109d78ab30a4e52055
vivienld <leducvivien@gmail.com>
2022-06-16 14:15:07 +0200

    test: removed test causing too many requests

--- binance4j-wallet/src/test/java/com/binance4j/client/GetMarginAccountSnapshotTest.java
+++ binance4j-wallet/src/test/java/com/binance4j/client/GetMarginAccountSnapshotTest.java
@@ -3,12 +3,10 @@ package com.binance4j.client;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import com.binance4j.core.exception.ApiException;
 import com.binance4j.service.TestService;
 import com.binance4j.wallet.client.WalletClient;
 import com.binance4j.wallet.snapshot.AccountSnapshotRequest;
@@ -17,17 +15,19 @@ import com.binance4j.wallet.snapshot.margin.MarginAccountSnapshotResponse;
 class GetMarginAccountSnapshotTest {
 	final WalletClient client = TestService.CLIENT;
 
-	@Test
-	@DisplayName("It should return the snapshot or throw an exception if you don't have a margin account")
-	void testGetMarginAccountSnapshot() {
-		try {
-			MarginAccountSnapshotResponse snapshot = client.getMarginAccountSnapshot().execute();
-			test(snapshot);
-		} catch (ApiException e) {
+	// @Test
+	// @DisplayName("It should return the snapshot or throw an exception if you
+	// don't have a margin account")
+	// void testGetMarginAccountSnapshot() {
+	// try {
+	// MarginAccountSnapshotResponse snapshot =
+	// client.getMarginAccountSnapshot().execute();
+	// test(snapshot);
+	// } catch (ApiException e) {
 
-			assertTrue(true);
-		}
-	}
+	// assertTrue(true);
+	// }
+	// }
 
 	@Test
 	@DisplayName("It should return the snapshot with the good size or throw an exception if you don't have a margin account")
