75130c840c8a722a927e30302bd1bf9ad16802d1
vivienld <leducvivien@gmail.com>
2022-07-05 10:25:55 +0200

    test: added test

---
+++ binance4j-strategy/src/test/java/com/binance4j/strategy/NoResponseTimeoutTest.java
@@ -0,0 +1,81 @@
+package com.binance4j.strategy;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.binance4j.core.market.CandlestickInterval;
+import com.binance4j.core.test.ConcurrentTest;
+import com.binance4j.strategy.dto.StrategyCallback;
+import com.binance4j.strategy.service.WatchService;
+import com.binance4j.strategy.strategies.AlwaysEnterStrategy;
+import com.binance4j.websocket.candle.WebsocketCandlestickClient;
+
+class NoResponseTimeoutTest extends ConcurrentTest {
+	int count;
+	final StrategyCallback callback;
+	CompletableFuture<Boolean> future;
+	AlwaysEnterStrategy strategy;
+	WatchService service;
+
+	long startTime;
+
+	NoResponseTimeoutTest() {
+		callback = new StrategyCallback();
+
+		callback.onOpen(t -> {
+			startTime = System.currentTimeMillis();
+		});
+
+		callback.onClosed(t -> {
+			assertNotNull(t);
+			future.complete(true);
+		});
+
+		callback.onFailure(t -> {
+			assertNotNull(t);
+			future.complete(true);
+		});
+
+		callback.onEnter(t -> {
+			assertNotNull(t);
+			service.unwatch();
+		});
+
+		callback.onExit(t -> {
+			assertNotNull(t);
+			service.unwatch();
+		});
+
+		callback.onFailure(t -> {
+			System.out.println(t);
+			System.out.println((System.currentTimeMillis() - startTime) / 1000);
+			service.unwatch();
+		});
+
+		callback.onMessage(Assertions::assertNotNull);
+	}
+
+	@BeforeEach
+	void beforeEach() {
+		count = 0;
+		future = new CompletableFuture<>();
+		strategy = new AlwaysEnterStrategy();
+		service = new WatchService(strategy);
+	}
+
+	@Test
+	@DisplayName("The client should receive data at every tick and should enter at first final bar")
+	void test() throws InterruptedException, ExecutionException {
+		WebsocketCandlestickClient client = service.watch("BNBBTC", CandlestickInterval.ONE_MINUTE, callback);
+		assertTrue(future.get());
+	}
+
+}
