ba8108b5c7d0a0166caf172dd3c5eefb48aa1627
vivienld <leducvivien@gmail.com>
2022-07-10 01:36:35 +0200

    test: corrected tests

--- binance4j-market/src/test/java/com/binance4j/market/GetExchangeInfoTest.java
+++ binance4j-market/src/test/java/com/binance4j/market/GetExchangeInfoTest.java
@@ -1,31 +1,24 @@
 package com.binance4j.market;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import com.binance4j.core.exception.ApiException;
 import com.binance4j.core.exchangeinfo.ExchangeInfo;
 import com.binance4j.core.exchangeinfo.ExchangeInfoRequest;
 
 class GetExchangeInfoTest extends MarketTest {
-	// @Test
-	// TODO Correct test
+	@Test
 	@DisplayName("Response must not contain null properties")
 	void test1() throws ApiException {
-		ExchangeInfo res = client.getExchangeInfo().execute();
-		System.out.println(res);
-		test(res);
+		test(client.getExchangeInfo().execute());
 	}
 
-	// @Test
-	// TODO Correct test
+	@Test
 	@DisplayName("Response must not contain null properties")
 	void test2() throws ApiException {
 		ExchangeInfoRequest req = new ExchangeInfoRequest(symbol);
 		ExchangeInfo res = client.getExchangeInfo(req).execute();
-
-		assertEquals(res.getSymbolInfo(symbol).getSymbol(), symbol);
 		test(res);
 	}
 }
--- binance4j-wallet/src/test/java/com/binance4j/client/GetWithdrawHistoryTest.java
+++ binance4j-wallet/src/test/java/com/binance4j/client/GetWithdrawHistoryTest.java
@@ -1,16 +1,30 @@
 package com.binance4j.client;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.binance4j.core.exception.ApiException;
+import com.binance4j.wallet.withdraw.WithdrawHistory;
 import com.binance4j.wallet.withdraw.WithdrawHistoryRequest;
 import com.binance4j.wallet.withdraw.WithdrawStatus;
 
 class GetWithdrawHistoryTest extends WalletTest {
-	// @Test
-	// TODO correct test
-	@DisplayName("It should return the WithdrawHistory")
+
+	// withdrawOrderId may be absent from the withdraw
+	@Override
+	public void test(Object bean) {
+		assertTrue(getNullProperties(bean).stream()
+				.filter(prop -> !prop.contains("withdrawOrderId")).collect(Collectors.toList()).isEmpty());
+	}
+
+	@Test
+	@DisplayName("Response properties should not be null except withdrawOrderId")
 	void testGetWithdrawHistory() throws ApiException {
 		test(client.getWithdrawHistory().execute());
 	}
@@ -24,6 +38,14 @@ class GetWithdrawHistoryTest extends WalletTest {
 	@Test
 	@DisplayName("It should return the WithdrawHistory of the given asset and status")
 	void testGetWithdrawHistoryWithAssetAndStatus() throws ApiException {
-		test(new WithdrawHistoryRequest(getAsset(), WithdrawStatus.COMPLETED));
+		WithdrawStatus status = WithdrawStatus.COMPLETED;
+		List<WithdrawHistory> history = client.getWithdrawHistory(new WithdrawHistoryRequest(getAsset(), status))
+				.execute();
+
+		test(history);
+
+		history.forEach(h -> assertEquals(getAsset(), h.getCoin()));
+		history.forEach(h -> assertEquals(status.getValue(), h.getStatus()));
+		test(history);
 	}
 }
