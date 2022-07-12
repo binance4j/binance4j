74bd96c26260c9939cd23d3f49ae85445212e67a
vivienld <leducvivien@gmail.com>
2022-07-13 00:41:04 +0200

    test: updated test

--- binance4j-market/src/test/java/com/binance4j/market/GetExchangeInfoTest.java
+++ binance4j-market/src/test/java/com/binance4j/market/GetExchangeInfoTest.java
@@ -1,24 +1,36 @@
 package com.binance4j.market;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.Set;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.binance4j.core.exception.ApiException;
-import com.binance4j.core.exchangeinfo.ExchangeInfo;
 import com.binance4j.core.exchangeinfo.ExchangeInfoRequest;
 
 class GetExchangeInfoTest extends MarketTest {
+
+	@Override
+	public void test(Object bean) {
+		Set<String> nulls = getNullProperties(bean, true);
+		System.out.println(nulls);
+		assertTrue(
+				nulls.containsAll(
+						Arrays.asList("notional", "percentPriceBySide", "maxPosition", "maxNumIcebergOrders")));
+	}
+
 	@Test
 	@DisplayName("Response must not contain null properties")
 	void test1() throws ApiException {
-		test(client.getExchangeInfo());
+		test(client.getExchangeInfo().execute());
 	}
 
 	@Test
 	@DisplayName("Response must not contain null properties")
 	void test2() throws ApiException {
-		ExchangeInfoRequest req = new ExchangeInfoRequest(symbol);
-		ExchangeInfo res = client.getExchangeInfo(req).execute();
-		test(res);
+		test(client.getExchangeInfo(new ExchangeInfoRequest(symbol)).execute());
 	}
 }
--- binance4j-wallet/src/test/java/com/binance4j/client/GetMarginAccountSnapshotTest.java
+++ binance4j-wallet/src/test/java/com/binance4j/client/GetMarginAccountSnapshotTest.java
@@ -1,17 +1,22 @@
 package com.binance4j.client;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.binance4j.core.exception.ApiException;
 import com.binance4j.wallet.snapshot.AccountSnapshotRequest;
+import com.binance4j.wallet.snapshot.margin.MarginAccountSnapshotResponse;
 
 class GetMarginAccountSnapshotTest extends WalletTest {
 
 	@Test
 	@DisplayName("It should return the snapshot with the good size or throw an exception if you don't have a margin account")
-	void testMarginAccountSnapshotWithLimit() {
-		assertDoesNotThrow(() -> test(client.getMarginAccountSnapshot(new AccountSnapshotRequest(limit))));
+	void testMarginAccountSnapshotWithLimit() throws ApiException {
+		MarginAccountSnapshotResponse res = client.getMarginAccountSnapshot(new AccountSnapshotRequest(limit))
+				.execute();
+		assertDoesNotThrow(() -> assertTrue(getNullProperties(res, true).contains("marginLevel")));
 	}
 }
