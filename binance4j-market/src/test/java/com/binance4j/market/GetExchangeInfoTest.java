d5bd39e2553276b127a073489e72b89fb3e7ece9
vivienld <leducvivien@gmail.com>
2022-07-21 21:19:10 +0200

    fix: moved params from core to market

--- binance4j-market/src/test/java/com/binance4j/market/GetExchangeInfoTest.java
+++ binance4j-market/src/test/java/com/binance4j/market/GetExchangeInfoTest.java
@@ -8,7 +8,7 @@ import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 import com.binance4j.core.exception.ApiException;
-import com.binance4j.core.param.ExchangeInfoParams;
+import com.binance4j.market.param.ExchangeInfoParams;
 
 class GetExchangeInfoTest extends MarketTest {
 
