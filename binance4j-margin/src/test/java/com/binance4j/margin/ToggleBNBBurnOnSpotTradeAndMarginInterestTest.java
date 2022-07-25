18bdc361d2e8e44659b08da8c9b91ab2ccff7d26
vivienld <leducvivien@gmail.com>
2022-07-25 04:04:25 +0200

    test: updated test

--- binance4j-margin/src/test/java/com/binance4j/margin/ToggleBNBBurnOnSpotTradeAndMarginInterestTest.java
+++ binance4j-margin/src/test/java/com/binance4j/margin/ToggleBNBBurnOnSpotTradeAndMarginInterestTest.java
@@ -1,11 +1,24 @@
 package com.binance4j.margin;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 
+import com.binance4j.core.Request;
 import com.binance4j.core.exception.ApiException;
 import com.binance4j.margin.param.ToggleBurnParams;
 
 public class ToggleBNBBurnOnSpotTradeAndMarginInterestTest extends MarginTest {
+
+	@Override
+	public void test(Request<?> executor) throws ApiException {
+		try {
+			test(executor.execute());
+		} catch (ApiException e) {
+			assertNotNull(e.getMessage());
+		}
+	}
+
 	@Test
 	void test1() throws ApiException {
 		test(client.toggleBNBBurnOnSpotTradeAndMarginInterest(new ToggleBurnParams(true, true)));
