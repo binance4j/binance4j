144e4e393baf0eaf90c98ba174c530799eca5406
vivienld <leducvivien@gmail.com>
2022-07-28 11:22:02 +0200

    feat: moving Params to interface

---
+++ binance4j-core/src/main/java/com/binance4j/core/annotation/Param.java
@@ -0,0 +1,20 @@
+package com.binance4j.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.binance4j.core.dto.RateLimitType;
+import com.binance4j.core.param.Params;
+
+/** {@link Params} metadata. */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Param {
+	int weight() default 1;
+
+	RateLimitType type() default RateLimitType.IP;
+
+	boolean isOrder() default false;
+}
---
+++ binance4j-core/src/main/java/com/binance4j/core/param/CallParams.java
@@ -0,0 +1,79 @@
+package com.binance4j.core.param;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import com.binance4j.core.annotation.Mandatory;
+import com.binance4j.core.annotation.Param;
+import com.binance4j.core.dto.RateLimitType;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import retrofit2.http.QueryMap;
+
+/** The base of every Binance Request */
+public interface CallParams {
+	/** Jackson object mapper used to convert a POJO into a {@link QueryMap} */
+	ObjectMapper defaultMapper = new ObjectMapper();
+	/** The receiving window. */
+	Long defaultRecvWindow = 60_000L;
+
+	default ObjectMapper mapper() {
+		return defaultMapper;
+	}
+
+	default long recvWindow() {
+		return defaultRecvWindow;
+	}
+
+	default long timestamp() {
+		return System.currentTimeMillis();
+	}
+
+	default int weight() {
+		try {
+			return getClass().getAnnotation(Param.class).weight();
+		} catch (Exception e) {
+			return 1;
+		}
+	}
+
+	default RateLimitType rateLimitType() {
+		try {
+			return getClass().getAnnotation(Param.class).type();
+		} catch (Exception e) {
+			return RateLimitType.IP;
+		}
+	}
+
+	default boolean isOrder() {
+		try {
+			return getClass().getAnnotation(Param.class).isOrder();
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	/**
+	 * @return the params into a {@link QueryMap} and removes null and useless parameters.
+	 */
+	default Map<String, Object> toMap() {
+		Map<String, Object> map = mapper().convertValue(this, new TypeReference<Map<String, Object>>() {
+		});
+
+		List.of("weight", "weightType", "order").forEach(f -> map.remove(f));
+		map.put("timestamp", timestamp());
+		map.put("recvWindow", recvWindow());
+		map.values().removeAll(Collections.singleton(null));
+		return map;
+	}
+
+	/**
+	 * @return The param fields annotated with {@link Mandatory}.
+	 */
+	default List<Field> mandatoryFields() {
+		return List.of(getClass().getDeclaredFields()).stream().filter(f -> f.isAnnotationPresent(Mandatory.class)).toList();
+	}
+}
