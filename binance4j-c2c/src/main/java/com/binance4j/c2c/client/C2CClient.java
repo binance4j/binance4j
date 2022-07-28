dd6fe7ef87a995eb2a1877f3595564e7b177cd81
vivienld <leducvivien@gmail.com>
2022-07-28 19:40:48 +0200

    refacto: Params are now records

--- binance4j-c2c/src/main/java/com/binance4j/c2c/client/C2CClient.java
+++ binance4j-c2c/src/main/java/com/binance4j/c2c/client/C2CClient.java
@@ -4,6 +4,7 @@ import com.binance4j.c2c.dto.TradeHistory;
 import com.binance4j.c2c.param.TradeHistoryParams;
 import com.binance4j.core.Request;
 import com.binance4j.core.client.RestClient;
+import com.binance4j.core.param.Pagination;
 
 /**
  * Api client for the c2c endpoints
@@ -17,6 +18,17 @@ public class C2CClient extends RestClient<C2CMapping> {
 	 */
 	public C2CClient(String key, String secret) {
 		super(C2CMapping.class, key, secret);
+	}
+
+	/**
+	 * Get C2C trades.
+	 * 
+	 * @param params     The request params.
+	 * @param pagination The pagination search.
+	 * @return The request to execute.
+	 */
+	public Request<TradeHistory> getTrades(TradeHistoryParams params, Pagination pagination) {
+		return new Request<>(service.getTrades(params.toMap(pagination)));
 	}
 
 	/**
--- binance4j-c2c/src/main/java/com/binance4j/c2c/param/TradeHistoryParams.java
+++ binance4j-c2c/src/main/java/com/binance4j/c2c/param/TradeHistoryParams.java
@@ -3,66 +3,14 @@ package com.binance4j.c2c.param;
 import com.binance4j.c2c.client.C2CClient;
 import com.binance4j.c2c.dto.TradeType;
 import com.binance4j.core.annotation.Mandatory;
+import com.binance4j.core.annotation.Param;
 import com.binance4j.core.param.Params;
 
-/** The {@link C2CClient#getTrades} params. */
-public class TradeHistoryParams implements Params {
-
-	/** Trade type. */
-	@Mandatory
-	TradeType tradeType;
-	/** Page. Default: 1. */
-	Integer page;
-	/** Rows. Default 100. Max 100. */
-	Integer rows;
-
-	/**
-	 * @param tradeType Trade type.
-	 */
-	public TradeHistoryParams(TradeType tradeType) {
-
-		this.tradeType = tradeType;
-	}
-
-	/**
-	 * @return the page
-	 */
-	public Integer getPage() {
-		return page;
-	}
-
-	/**
-	 * @param page the page to set
-	 */
-	public void setPage(Integer page) {
-		this.page = page;
-	}
-
-	/**
-	 * @return the rows
-	 */
-	public Integer getRows() {
-		return rows;
-	}
-
-	/**
-	 * @param rows the rows to set
-	 */
-	public void setRows(Integer rows) {
-		this.rows = rows;
-	}
-
-	/**
-	 * @return the tradeType
-	 */
-	public TradeType getTradeType() {
-		return tradeType;
-	}
-
-	/**
-	 * @param tradeType the tradeType to set
-	 */
-	public void setTradeType(TradeType tradeType) {
-		this.tradeType = tradeType;
-	}
+/**
+ * The {@link C2CClient#getTrades} params.
+ * 
+ * @param tradeType Trade type.
+ */
+@Param
+public record TradeHistoryParams(@Mandatory TradeType tradeType) implements Params {
 }
