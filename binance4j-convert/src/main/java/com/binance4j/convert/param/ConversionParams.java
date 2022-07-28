be2b4ca3305ec373be6eccd401de1b1df2ae4ff1
vivienld <leducvivien@gmail.com>
2022-07-28 19:44:02 +0200

    refacto: Params are now records

--- binance4j-convert/src/main/java/com/binance4j/convert/param/ConversionParams.java
+++ binance4j-convert/src/main/java/com/binance4j/convert/param/ConversionParams.java
@@ -7,69 +7,15 @@ import com.binance4j.core.param.Params;
 
 /**
  * The {@link ConvertClient#getConversions} params.
+ * 
+ * @param startTime The starting time in ms.
+ * @param endTime   The ending times in ms.
+ * @param limit     The result size. Default 100, Max 1000.
  */
 @Param(weight = 100)
-public class ConversionParams implements Params {
-	/** The starting timestamp of the results */
-	@Mandatory
-	Long startTime;
-	/** The ending timestamp of the results */
-	@Mandatory
-	Long endTime;
-
-	/** The result size. Default 100, Max 1000. */
-	Integer limit;
-
-	/**
-	 * @param startTime Start time in ms.
-	 * @param endTime   End time in ms.
-	 */
-	public ConversionParams(long startTime, long endTime) {
-		this.startTime = startTime;
-		this.endTime = endTime;
-	}
-
-	/**
-	 * @param startTime Start time in ms.
-	 * @param endTime   End time in ms.
-	 * @param limit     The results limit.
-	 */
-	public ConversionParams(long startTime, long endTime, int limit) {
-		this(startTime, endTime);
-		this.limit = limit;
-	}
-
-	/**
-	 * @return the limit.
-	 */
-	public Integer getLimit() {
-		return limit;
-	}
-
-	/**
-	 * @param limit the limit to set.
-	 */
-	public void setLimit(Integer limit) {
-		this.limit = limit;
-	}
-
-	/** @return the startTime */
-	public Long getStartTime() {
-		return startTime;
-	}
-
-	/** @param startTime the startTime to set */
-	public void setStartTime(Long startTime) {
-		this.startTime = startTime;
-	}
-
-	/** @return the endTime */
-	public Long getEndTime() {
-		return endTime;
-	}
-
-	/** @param endTime the endTime to set */
-	public void setEndTime(Long endTime) {
-		this.endTime = endTime;
+public record ConversionParams(@Mandatory Long startTime, @Mandatory Long endTime, Integer limit) implements Params {
+	/** Creates instance of {@link ConversionParams}. */
+	public ConversionParams(Long startTime, Long endTime) {
+		this(startTime, endTime, null);
 	}
 }
