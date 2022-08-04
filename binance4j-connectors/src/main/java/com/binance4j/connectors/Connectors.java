61d2afccd217a6ef60296dacd63b16c94cfca26d
vivienld <leducvivien@gmail.com>
2022-08-04 08:12:11 +0200

    feat: Connectors class

---
+++ binance4j-connectors/src/main/java/com/binance4j/connectors/Connectors.java
@@ -0,0 +1,300 @@
+package com.binance4j.connectors;
+
+import com.binance4j.blvt.client.BLVTClient;
+import com.binance4j.c2c.client.C2CClient;
+import com.binance4j.fiat.client.FiatClient;
+import com.binance4j.loan.client.LoanClient;
+import com.binance4j.margin.client.MarginClient;
+import com.binance4j.market.client.MarketClient;
+import com.binance4j.mining.client.MiningClient;
+import com.binance4j.nft.client.NFTClient;
+import com.binance4j.pay.client.PayClient;
+import com.binance4j.portfoliomargin.client.PortfolioMarginClient;
+import com.binance4j.rebate.client.RebateClient;
+import com.binance4j.savings.client.SavingsClient;
+import com.binance4j.spot.client.SpotClient;
+import com.binance4j.staking.client.StakingClient;
+import com.binance4j.vision.client.VisionSpotClient;
+import com.binance4j.wallet.client.WalletClient;
+
+/** Connectors container. */
+public class Connectors {
+	/** The API key */
+	protected String key;
+	/** The API secret */
+	protected String secret;
+
+	/** Inner {@link BLVTClient}. */
+	protected BLVTClient blvtClient;
+	/** Inner {@link C2CClient}. */
+	protected C2CClient c2cClient;
+	/** Inner {@link FiatClient}. */
+	protected FiatClient fiatClient;
+	/** Inner {@link LoanClient}. */
+	protected LoanClient loanClient;
+	/** Inner {@link MarginClient}. */
+	protected MarginClient marginClient;
+	/** Inner {@link MarketClient}. */
+	protected MarketClient marketClient;
+	/** Inner {@link MiningClient}. */
+	protected MiningClient miningClient;
+	/** Inner {@link NFTClient}. */
+	protected NFTClient nftClient;
+	/** Inner {@link PayClient}. */
+	protected PayClient payClient;
+	/** Inner {@link PortfolioMarginClient}. */
+	protected PortfolioMarginClient portfolioMarginClient;
+	/** Inner {@link RebateClient}. */
+	protected RebateClient rebateClient;
+	/** Inner {@link SavingsClient}. */
+	protected SavingsClient savingsClient;
+	/** Inner {@link SpotClient}. */
+	protected SpotClient spotClient;
+	/** Inner {@link StakingClient}. */
+	protected StakingClient stakingClient;
+	/** Inner {@link VisionSpotClient}. */
+	protected VisionSpotClient visionSpotClient;
+	/** Inner {@link WalletClient}. */
+	protected WalletClient walletClient;
+
+	/**
+	 * @param key    The API key
+	 * @param secret The API secret
+	 */
+	public Connectors(String key, String secret) {
+		this.key = key;
+		this.secret = secret;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link BLVTClient}.
+	 * 
+	 * @return an instance of {@link BLVTClient}.
+	 */
+	public BLVTClient blvtClient() {
+		if (blvtClient == null) {
+			blvtClient = new BLVTClient(key, secret);
+		}
+		blvtClient.updateKeys(key, secret);
+		return blvtClient;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link FiatClient}.
+	 * 
+	 * @return an instance of {@link FiatClient}.
+	 */
+	public C2CClient c2cClient() {
+		if (c2cClient == null) {
+			c2cClient = new C2CClient(key, secret);
+		}
+		c2cClient.updateKeys(key, secret);
+		return c2cClient;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link FiatClient}.
+	 * 
+	 * @return an instance of {@link FiatClient}.
+	 */
+	public FiatClient fiatClient() {
+		if (fiatClient == null) {
+			fiatClient = new FiatClient(key, secret);
+		}
+		fiatClient.updateKeys(key, secret);
+		return fiatClient;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link LoanClient}.
+	 * 
+	 * @return an instance of {@link LoanClient}.
+	 */
+	public LoanClient loanClient() {
+		if (loanClient == null) {
+			loanClient = new LoanClient(key, secret);
+		}
+		loanClient.updateKeys(key, secret);
+		return loanClient;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link MarginClient}.
+	 * 
+	 * @return an instance of {@link MarginClient}.
+	 */
+	public MarginClient marginClient() {
+		if (marginClient == null) {
+			marginClient = new MarginClient(key, secret);
+		}
+		marginClient.updateKeys(key, secret);
+		return marginClient;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link MarketClient}.
+	 * 
+	 * @return an instance of {@link MarketClient}.
+	 */
+	public MarketClient marketClient() {
+		if (marketClient == null) {
+			marketClient = new MarketClient(key, secret);
+		}
+		marketClient.updateKeys(key, secret);
+		return marketClient;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link MiningClient}.
+	 * 
+	 * @return an instance of {@link MiningClient}.
+	 */
+	public MiningClient miningClient() {
+		if (miningClient == null) {
+			miningClient = new MiningClient(key, secret);
+		}
+		miningClient.updateKeys(key, secret);
+		return miningClient;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link NFTClient}.
+	 * 
+	 * @return an instance of {@link NFTClient}.
+	 */
+	public NFTClient nftClient() {
+		if (nftClient == null) {
+			nftClient = new NFTClient(key, secret);
+		}
+		nftClient.updateKeys(key, secret);
+		return nftClient;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link PayClient}.
+	 * 
+	 * @return an instance of {@link PayClient}.
+	 */
+	public PayClient payClient() {
+		if (payClient == null) {
+			payClient = new PayClient(key, secret);
+		}
+		payClient.updateKeys(key, secret);
+		return payClient;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link PortfolioMarginClient}.
+	 * 
+	 * @return an instance of {@link PortfolioMarginClient}.
+	 */
+	public PortfolioMarginClient portfoliomarginClient() {
+		if (portfolioMarginClient == null) {
+			portfolioMarginClient = new PortfolioMarginClient(key, secret);
+		}
+		portfolioMarginClient.updateKeys(key, secret);
+		return portfolioMarginClient;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link RebateClient}.
+	 * 
+	 * @return an instance of {@link RebateClient}.
+	 */
+	public RebateClient rebateClient() {
+		if (rebateClient == null) {
+			rebateClient = new RebateClient(key, secret);
+		}
+		rebateClient.updateKeys(key, secret);
+		return rebateClient;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link SavingsClient}.
+	 * 
+	 * @return an instance of {@link SavingsClient}.
+	 */
+	public SavingsClient savingsClient() {
+		if (savingsClient == null) {
+			savingsClient = new SavingsClient(key, secret);
+		}
+		savingsClient.updateKeys(key, secret);
+		return savingsClient;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link SpotClient}.
+	 * 
+	 * @return an instance of {@link SpotClient}.
+	 */
+	public SpotClient spotClient() {
+		if (spotClient == null) {
+			spotClient = new SpotClient(key, secret);
+		}
+		spotClient.updateKeys(key, secret);
+		return spotClient;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link StakingClient}.
+	 * 
+	 * @return an instance of {@link StakingClient}.
+	 */
+	public StakingClient stakingClient() {
+		if (stakingClient == null) {
+			stakingClient = new StakingClient(key, secret);
+		}
+		stakingClient.updateKeys(key, secret);
+		return stakingClient;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link VisionSpotClient}.
+	 * 
+	 * @return an instance of {@link VisionSpotClient}.
+	 */
+	public VisionSpotClient visionSpotClient() {
+		if (visionSpotClient == null) {
+			visionSpotClient = new VisionSpotClient();
+		}
+		return visionSpotClient;
+	}
+
+	/**
+	 * Lazy loads and returns a cached instance of {@link WalletClient}.
+	 * 
+	 * @return an instance of {@link WalletClient}.
+	 */
+	public WalletClient walletClient() {
+		if (walletClient == null) {
+			walletClient = new WalletClient(key, secret);
+		}
+		walletClient.updateKeys(key, secret);
+		return walletClient;
+	}
+
+	/**
+	 * Updates the API keys.
+	 * 
+	 * @param key    The new public key.
+	 * @param secret The new secret key.
+	 */
+	public void updateKeys(String key, String secret) {
+		this.key = key;
+		this.secret = secret;
+	}
+
+	/**
+	 * @return the key
+	 */
+	public String getKey() {
+		return key;
+	}
+
+	/**
+	 * @return the secret
+	 */
+	public String getSecret() {
+		return secret;
+	}
+}
---
+++ binance4j-connectors/src/test/java/com/binance4j/connectors/ConnectorsTest.java
@@ -0,0 +1,106 @@
+package com.binance4j.connectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+import com.binance4j.core.exception.ApiException;
+import com.binance4j.core.test.CustomTest;
+
+public class ConnectorsTest extends CustomTest {
+	Connectors connectors;
+
+	public ConnectorsTest() {
+		connectors = new Connectors(key, secret);
+	}
+
+	@Test
+	void testGetBLVTClient() throws ApiException {
+		assertNotNull(connectors.blvtClient());
+	}
+
+	@Test
+	void testGetC2CClient() {
+		assertNotNull(connectors.c2cClient());
+	}
+
+	@Test
+	void testGetFiatClient() {
+		assertNotNull(connectors.fiatClient());
+	}
+
+	@Test
+	void testGetLoanClient() {
+		assertNotNull(connectors.loanClient());
+	}
+
+	@Test
+	void testGetMarginClient() {
+		assertNotNull(connectors.marginClient());
+	}
+
+	@Test
+	void testGetMarketClient() {
+		assertNotNull(connectors.marketClient());
+	}
+
+	@Test
+	void testGetMiningClient() {
+		assertNotNull(connectors.miningClient());
+	}
+
+	@Test
+	void testGetNFTClient() {
+		assertNotNull(connectors.nftClient());
+	}
+
+	@Test
+	void testGetPayClient() {
+		assertNotNull(connectors.payClient());
+	}
+
+	@Test
+	void testGetPortfolioMarginClient() {
+		assertNotNull(connectors.marginClient());
+	}
+
+	@Test
+	void testGetRebateClient() {
+		assertNotNull(connectors.rebateClient());
+	}
+
+	@Test
+	void testGetSavingsClient() {
+		assertNotNull(connectors.savingsClient());
+	}
+
+	@Test
+	void testGetSpotClient() {
+		assertNotNull(connectors.spotClient());
+	}
+
+	@Test
+	void testGetStakingClient() {
+		assertNotNull(connectors.stakingClient());
+	}
+
+	@Test
+	void testGetVisionSpotClient() {
+		assertNotNull(connectors.visionSpotClient());
+	}
+
+	@Test
+	void testGetWalletClient() {
+		assertNotNull(connectors.walletClient());
+	}
+
+	@Test
+	void testUpdateKeys() {
+		assertEquals(connectors.getKey(), connectors.blvtClient().getKey());
+		assertEquals(connectors.getSecret(), connectors.blvtClient().getSecret());
+		connectors.updateKeys("foo", "bar");
+		assertEquals(connectors.getKey(), connectors.blvtClient().getKey());
+		assertEquals(connectors.getSecret(), connectors.blvtClient().getSecret());
+	}
+}
