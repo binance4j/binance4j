289c9498c37a4895afd02c9fafb273d98581feb4
vivienld <leducvivien@gmail.com>
2022-08-07 10:32:07 +0200

    feat: added ConvertController

--- binance4j-web/src/main/java/com/binance4j/web/controller/ConvertController.java
+++ binance4j-web/src/main/java/com/binance4j/web/controller/ConvertController.java
@@ -5,19 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.binance4j.c2c.dto.TradeHistory;
-import com.binance4j.c2c.dto.TradeType;
-import com.binance4j.c2c.param.TradeHistoryParams;
 import com.binance4j.connectors.Connectors;
 import com.binance4j.convert.client.ConvertClient;
+import com.binance4j.convert.dto.ConversionHistory;
 import com.binance4j.core.exception.ApiException;
+import com.binance4j.core.param.TimeFrame;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
 @RestController
-@RequestMapping("api/v1/c2c")
+@RequestMapping("api/v1/convert")
 @Api(value = "Convert Controller", tags = "Convert Controller", produces = "application/json", description = "Convert endpoints")
 public class ConvertController {
 
@@ -27,9 +26,11 @@ public class ConvertController {
 		this.client = connectors.convert();
 	}
 
-	@GetMapping(path = "trades", produces = "application/json")
-	@ApiOperation(value = "Get trades.")
-	public TradeHistory getTrades(@RequestParam(required = true) @ApiParam(example = "BUY", value = "The trade type") TradeType tradeType) throws ApiException {
-		return client.getTrades(new TradeHistoryParams(tradeType)).sync();
+	@GetMapping(path = "conversions", produces = "application/json")
+	@ApiOperation(value = "Get conversions.")
+	public ConversionHistory getTrades(@RequestParam(required = false) @ApiParam(example = "1659824617000", value = "Start time in ms") Long startTime,
+			@RequestParam(required = false) @ApiParam(example = "1659824617000", value = "End time in ms") Long endTime,
+			@RequestParam(required = false) @ApiParam(example = "25", value = "The result limit") Integer limit) throws ApiException {
+		return client.getConversions(new TimeFrame(startTime, endTime, limit)).sync();
 	}
 }
