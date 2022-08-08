df0314e3bc1c395ed5bad41ee50f56795e7d31a3
vivienld <leducvivien@gmail.com>
2022-08-08 04:32:18 +0200

    feat: updated controller

--- binance4j-web/src/main/java/com/binance4j/web/controller/StakingControllerTodo.java
+++ binance4j-web/src/main/java/com/binance4j/web/controller/StakingControllerTodo.java
@@ -9,12 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.binance4j.core.exception.ApiException;
 import com.binance4j.core.param.FramedPaging;
+import com.binance4j.core.param.Paging;
 import com.binance4j.staking.dto.LeftQuota;
+import com.binance4j.staking.dto.Product;
 import com.binance4j.staking.dto.ProductType;
 import com.binance4j.staking.dto.StakingRecord;
 import com.binance4j.staking.dto.TransactionType;
 import com.binance4j.staking.param.HistoryParams;
 import com.binance4j.staking.param.LeftQuotaParams;
+import com.binance4j.staking.param.ProductListParams;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -25,6 +28,23 @@ import io.swagger.annotations.ApiParam;
 @RequestMapping("api/v1/staking")
 @Api(value = "Staking", tags = "Staking", produces = "application/json", description = "Staking endpoints")
 public class StakingControllerTodo extends BaseController {
+
+	/**
+	 * @param product Product type.
+	 * @param asset   Product name.
+	 * @param page    Results page.
+	 * @param limit   Results limit.
+	 * @return Available Staking product list.
+	 * @throws ApiException Something went wrong with the API.
+	 */
+	@GetMapping(path = "products", produces = "application/json", params = { "product", "txnType", "asset" })
+	@ApiOperation(value = "Get available Staking product list.")
+	public List<Product> getProducts(@RequestParam(required = true) @ApiParam(example = "STAKING", value = "Product type.") ProductType product,
+			@RequestParam(required = false) @ApiParam(example = "BNB", value = "Product name.") String asset,
+			@RequestParam(required = false) @ApiParam(example = "1", value = "Results page.") Integer page,
+			@RequestParam(required = false) @ApiParam(example = "25", value = "The result limit.") Integer limit) throws ApiException {
+		return connectors.staking().getProducts(new ProductListParams(product, asset), new Paging(page, limit)).sync();
+	}
 
 	/**
 	 * @param product   Product type.
