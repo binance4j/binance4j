5d8b0dbe7ab086e401cf8f2b7345642eb9563920
vivienld <leducvivien@gmail.com>
2022-08-10 11:18:08 +0200

    test: added ApiRequest test

--- binance4j-core/src/test/java/com/binance4j/request/VoidRequest.java
+++ binance4j-core/src/test/java/com/binance4j/request/VoidRequest.java
@@ -1,9 +1,14 @@
 package com.binance4j.request;
 
 import com.binance4j.core.Request;
+import com.binance4j.core.annotation.ApiRequest;
+import com.binance4j.core.dto.HttpMethod;
+import com.binance4j.core.dto.RateLimitType;
+import com.binance4j.core.dto.Signature;
 
 import retrofit2.Call;
 
+@ApiRequest(isOrder = true, method = HttpMethod.HEAD, path = "/foo/bar/", rateLimit = RateLimitType.UID_AND_IP, signature = Signature.USER_STREAM, weight = 10)
 public class VoidRequest extends Request<Void> {
 	/**
 	 * @param call the retrofit call
@@ -11,4 +16,9 @@ public class VoidRequest extends Request<Void> {
 	public VoidRequest(Call<Void> call) {
 		super(call);
 	}
+
+	public VoidRequest() {
+		super(null);
+	}
+
 }
